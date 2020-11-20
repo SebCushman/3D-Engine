8c915d700361187c7e43fc080913bb5678cbf71c
SebCushman <61478231+SebCushman@users.noreply.github.com>
2020-11-20 13:43:21 -0700

    All changes up to 20 November, to much to mention here

--- Application/Main.cpp
+++ Application/Main.cpp
@@ -1,30 +1,56 @@
+#include "pch.h"
 #include <glad\glad.h>
-#include "sdl.h"
+#include "Engine\Graphics\Renderer.h"
+#include "Engine\Graphics\Program.h"
+#include "Engine/Graphics/Texture.h"
 
 int main(int argc, char** argv)
 {
-	int result = SDL_Init(SDL_INIT_VIDEO);
-	if (result != 0)
-	{
-		SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
-	}
+	nc::Renderer renderer;
+	renderer.Startup();
+	renderer.Create("OpenGL", 800, 600);
 
-	SDL_Window* window = SDL_CreateWindow("OpenGL", 100, 100, 800, 600, SDL_WINDOW_OPENGL);
-	if (window == nullptr)
-	{
-		SDL_Log("Failed to create window: %s", SDL_GetError());
-	}
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f,//point 1
+		 0.0f,  0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.5f, 1.0f,//point 2
+		 0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f //point 3
+	};
 
-	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-	SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
-	SDL_GL_SetSwapInterval(1);
+	nc::Program program;
+	program.CreateShaderFromFile("Shaders\\basic.vert", GL_VERTEX_SHADER);
+	program.CreateShaderFromFile("Shaders\\basic.frag", GL_FRAGMENT_SHADER);
+	program.Link();
+	program.Use();
 
-	SDL_GLContext context = SDL_GL_CreateContext(window);
-	if (!gladLoadGL())
-	{
-		SDL_Log("Failed to create OpenGL context");
-		exit(-1);
-	}
+	//create vertex buffers
+	GLuint vbo; //vertex buffer object
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//set position pipeline (vertex attribute)
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), 0);
+	glEnableVertexAttribArray(0);
+
+	//set color pipeline (vertex attribute)
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	//set UV pipeline (vertex attribute)
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+	//uniform
+	glm::mat4 transform = glm::mat4(1.0f);
+	//How to get it with the OpenGL code
+	program.SetUniform("transform", transform);
+
+	//without OpenGL code
+	/*GLuint uniform = glGetUniformLocation(program.GetProgramID(), "transform");
+	glUniformMatrix2fv(uniform, 1, GL_FALSE, glm::value_ptr(transform));*/
+	
+	nc::Texture texture;
+	texture.CreateTexture("Textures\\llama.jpg");
 
 
 	bool quit = false;
@@ -47,21 +73,21 @@ int main(int argc, char** argv)
 
 		SDL_PumpEvents();
 
-		glClearColor(0.85f, 0.85f, 0.85f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		/*glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);*/
 
-		glBegin(GL_TRIANGLES);
+		transform = glm::rotate(transform, 0.0004f, glm::vec3(0, 0, 1));
+		//How to get it with the OpenGL code
+		program.SetUniform("transform", transform);
+		
+		//without OpenGL code
+		//glUniformMatrix2fv(uniform, 1, GL_FALSE, glm::value_ptr(transform));
+		
+		renderer.BeginFrame();
 
-		glColor3f(1.0f, 0.0f, 0.0f);
-		glVertex2f(-0.5f, -0.5f);
-		glColor3f(0.0f, 1.0f, 0.0f);
-		glVertex2f(0.0f, 0.5f);
-		glColor3f(0.0f, 0.0f, 1.0f);
-		glVertex2f(0.5f, -0.5f);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
-		glEnd();
-
-		SDL_GL_SwapWindow(window);
+		renderer.EndFrame();
 	}
 
 	return 0;
